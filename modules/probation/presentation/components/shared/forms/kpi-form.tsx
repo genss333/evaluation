c1150657a74c404a8635ca8095d5429dc743f63e
Tatsapon Munsan <109135068+genss333@users.noreply.github.com>
2025-09-05 16:17:34 +0700

    feat: create ProbationDataTable component for improved data display in KPI form

--- modules/probation/presentation/components/shared/forms/kpi-form.tsx
+++ modules/probation/presentation/components/shared/forms/kpi-form.tsx
@@ -1,95 +1,16 @@
 "use client";
 
 import { TextField } from "@/components/ui/input";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { TabsContent } from "@/components/ui/tabs";
-import { cn } from "@/lib/utils";
 import {
   Kpi,
   KpiKey,
 } from "@/modules/probation/data/models/probation-kpi-model";
 import { useQuery } from "@tanstack/react-query";
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
+import { ColumnDef } from "@tanstack/react-table";
 import React, { useState } from "react";
 import { useFetchKpi } from "../../../hooks/fetch-probation";
-
-interface KpiDataTableProps {
-  columns: ColumnDef<Kpi>[];
-  data: Kpi[];
-}
-
-const KpiDataTable = ({ columns, data }: KpiDataTableProps) => {
-  const table = useReactTable({
-    data,
-    columns,
-    enableColumnResizing: true, // <-- เปิดใช้งาน
-    columnResizeMode: "onChange", // <-- เลือกว่าจะให้ re-render ตอนไหน
-    getCoreRowModel: getCoreRowModel(),
-  });
-
-  return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <TableHead
-                  key={header.id}
-                  style={{ width: header.getSize() }}
-                  className={cn(
-                    header.index == 1 || header.index == 2
-                      ? "text-left"
-                      : "text-center"
-                  )}
-                >
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
-                </TableHead>
-              ))}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  );
-};
+import ProbationDataTable from "../probation-data-table";
 
 const KpiForm = () => {
   const { data, isLoading } = useQuery(useFetchKpi());
@@ -222,7 +143,7 @@ const KpiForm = () => {
           <div className="font-body2 text-status-red">{data?.desc}</div>
         </div>
 
-        <KpiDataTable columns={columns} data={tableData || []} />
+        <ProbationDataTable columns={columns} data={tableData || []} />
       </div>
     </TabsContent>
   );
---
+++ modules/probation/presentation/components/shared/probation-data-table.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn } from "@/lib/utils";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+interface ProbationDataTableProps<T> {
+  columns: ColumnDef<T>[];
+  data: T[];
+}
+
+const ProbationDataTable: React.FC<ProbationDataTableProps<any>> = ({
+  columns,
+  data,
+}: ProbationDataTableProps<any>) => {
+  const table = useReactTable({
+    data,
+    columns,
+    enableColumnResizing: true, // <-- เปิดใช้งาน
+    columnResizeMode: "onChange", // <-- เลือกว่าจะให้ re-render ตอนไหน
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHead
+                  key={header.id}
+                  style={{ width: header.getSize() }}
+                  className={cn(
+                    header.index == 1 || header.index == 2
+                      ? "text-left"
+                      : "text-center"
+                  )}
+                >
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default ProbationDataTable;
